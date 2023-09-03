024db3b6d1d3b1add37007552a96d9b8f7c4e266
Pilgyeong Gwak <pid011@kakao.com>
2023-09-03 22:01:26 +0900

    Initial commit

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Hello!");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
