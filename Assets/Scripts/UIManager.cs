704472cfd6844a35ef1ed6743574f26aeee6f5bf
송종국 <openinhere1@gmail.com>
2023-09-14 01:07:57 +0900

    feat(JongGuck) Spine Set

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PanelType
+{
+    Title = 0,
+    Dialouage,
+    Lobby,
+    Stage1,
+    Stage2,
+    Setting
+}
+
+/// <summary>
+/// UI Panel을 등록하여 On/Off 관련된 관리합니다.
+/// </summary>
+public class UIManager : Singleton<UIManager>
+{
+    public PanelType panelType;
+
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
