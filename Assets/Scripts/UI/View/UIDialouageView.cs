7bb913b679adb2d8688e3da5cd7091559c067fe7
송종국 <openinhere1@gmail.com>
2023-09-17 02:37:16 +0900

    feat(main) ISkill Set

--- Assets/Scripts/UI/View/UIDialouageView.cs
+++ Assets/Scripts/UI/View/UIDialouageView.cs
@@ -11,7 +11,10 @@ public class UIDialouageView : UIView
 {
     [SerializeField] private TextMeshProUGUI ContentTxt;
     [SerializeField] private float typingSpeed;
-    [SerializeField] private string printContent; //#출력할 내용
+    [SerializeField] private string[] printContent; //#출력할 내용
+
+    private int currentIndex = 0; // 현재 출력 중인 문자열의 인덱스
+    private string currentText = ""; // 현재까지 타이핑된 텍스트
 
     private void Awake()
     {
@@ -22,8 +25,7 @@ public class UIDialouageView : UIView
     {
         base.Start();
 
-        SceneManager.LoadScene("UIScene");
-        SceneManager.LoadScene("Stage1Scene", LoadSceneMode.Additive);
+        StartCoroutine(TypeText());
     }
 
     public override void HidePanel()
@@ -35,10 +37,30 @@ public class UIDialouageView : UIView
     }
 
 
-    public IEnumerator TypingEffect()
+    private IEnumerator TypeText()
     {
-        //TODD : 한 글자씩 타이핑 or 문단마다 fade in/out
+        // 배열에서 문자열을 한 글자씩 출력
+        for (int i = 0; i < printContent[currentIndex].Length; i++)
+        {
+            currentText += printContent[currentIndex][i];
+            ContentTxt.text = currentText;
+            yield return new WaitForSeconds(typingSpeed);
+        }
 
-        yield return null;
+        // 다음 문자열을 출력하기 위해 인덱스 증가
+        currentIndex++;
+
+        // 모든 문자열을 출력한 경우 다음 작업을 수행하거나 반복 여부를 확인할 수 있습니다.
+        if (currentIndex < printContent.Length)
+        {
+            // 다음 문자열 출력을 시작
+            StartCoroutine(TypeText());
+        }
+        else
+        {
+            // 모든 문자열을 출력한 후에 실행할 작업을 수행 (예: 다음 장면으로 전환)
+            SceneManager.LoadScene("UIScene");
+            SceneManager.LoadScene("Stage1Scene", LoadSceneMode.Additive);
+        }
     }
 }
